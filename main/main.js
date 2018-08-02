be10063f8779760c31ae84cf03022d464ff70f3d
xuqiannew <949768312@qq.com>
2018-08-02 16:38:23 +0800

    3

--- main/main.js
+++ main/main.js
@@ -1,3 +1,8 @@
-module.exports = function main() {
+module.exports = function main(a) {
   // Write your code here
+  let arr = a.split('');
+  return sum = arr.reduce(function(prev , cur , index , array){
+    return parseInt(prev) + parseInt(cur) ;
+   });
+ 
 };
